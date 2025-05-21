5c5f3d625abbef8f6c5b238dc70c6e18b1fc81a4
pauolvez <pauoliveamazon@gmail.com>
2025-05-21 16:09:16 +0200

    Scraper de mierda
    Scraper de mierda

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,15 @@
+import asyncio
+import sys
 from fastapi import FastAPI, Query
 from app.auth import fastapi_users, auth_backend
 from app.users import UserRead, UserCreate, UserUpdate
 from app.models import User
 from app.database import Base, engine
-from app.scraper import buscar_productos_aliexpress  # 👈 Añadido
+from app.scraper import buscar_productos_aliexpress
+
+
+if sys.platform.startswith("win"):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 app = FastAPI()
 
@@ -12,7 +18,6 @@ app = FastAPI()
 async def on_startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
 
 # Rutas de autenticación
 app.include_router(
@@ -33,12 +38,9 @@ app.include_router(
     tags=["users"],
 )
 
-
 @app.get("/")
 def root():
     return {"mensaje": "Backend FastAPI funcionando correctamente con fastapi-users 14"}
-
-
 
 @app.get("/buscar-productos")
 async def buscar_productos(query: str = Query(..., min_length=2)):
